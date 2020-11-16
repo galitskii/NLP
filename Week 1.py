da81e6afd084933bb1905e8a64ef672003347446
galitskii <73939009+galitskii@users.noreply.github.com>
2020-11-16 22:35:21 +0300

    html:beginning

--- Week 1.py
+++ Week 1.py
@@ -1,56 +1,17 @@
-
 # coding: utf-8
-
 # *Работа с базой данных*
-
-# In[1]:
-
-
 # https://oracle.github.io/python-cx_Oracle/
 import cx_Oracle
-
-
-# In[10]:
-
-
 myconnection = cx_Oracle.connect('lexis/lexis@93.175.29.93/mipt')
-
-
-# In[11]:
-
-
 cur = myconnection.cursor()
-
-
-# In[12]:
-
-
 cur.execute("SELECT * FROM CASE_OF_WORD")
-
-
-# In[13]:
-
-
 rows = cur.fetchall()
-
-
-# In[14]:
-
-
 for row in rows:
         print("{0} {1}".format(row[0], row[1]))
-
-
-# In[15]:
-
-
 myconnection.close()
 
 
 # # токенайзер
-
-# In[20]:
-
 
 import re
 split_sent = re.compile(r'[.|!|?|…|?!]')
@@ -73,113 +34,92 @@ def s_words_tokenize(text):
     t = list(set(t))
     return t
 
-
-# In[21]:
-
-
 text = '''Министерство иностранных дел Азербайджана заявило, что российский вертолет сбили азербайджанские военные. Это произошло «в свете напряженной обстановки в регионе и повышенной боевой готовности в связи с возможными провокациями армянской стороны», сообщило ведомство.
 
 В МИДе пояснили, что полет вертолета проходил в непосредственной близости от армяно-азербайджанской границы на фоне продолжающихся военных действий в Нагорном Карабахе. Также в Баку отметили, что вертолет летел в темное время суток и что воздушная техника ВВС России ранее не была замечена в этом районе.
 
 Азербайджан «приносит извинения российской стороне в связи с данным трагическим инцидентом, который носит характер случайности». Баку также выразил соболезнования семьям погибших и пожелал скорейшего выздоровления пострадавшему. Азербайджан?!'''
 
-
-# In[22]:
-
-
 print(text)
-
-
-# In[23]:
-
 
 text = text.lower()
-
-
-# In[24]:
-
-
 print(text)
-
-
-# In[25]:
-
-
 sent = simple_sent_tokenize(text)
-
-
-# In[26]:
-
-
 print(sent)
-
-
-# In[27]:
-
-
 words = s_words_tokenize(sent)
-
-
-# In[28]:
-
-
 print(words)
 print("Количество слов в списке = ", len(words))
 
 
 # # алгоритм
-
-# In[6]:
-
-
 import cx_Oracle
-
-
-# In[7]:
-
-
 myconnection = cx_Oracle.connect('lexis/lexis@93.175.29.93/mipt')
-
-
-# In[14]:
-
-
 cur = myconnection.cursor()
-
-
-# In[15]:
-
-
 cur.execute("SELECT * FROM new_words")
-
-
-# In[18]:
-
-
-print(type(cur))
-
-
-# In[12]:
-
-
 rows = cur.fetchall()
-
-
-# In[17]:
-
-
-print(type(rows))
-
-
-# In[16]:
-
-
 for row in rows:
         print("{0} {1}".format(row[0], row[1]))
-
-
-# In[19]:
-
-
 myconnection.close()
 
+
+# # HTML
+# библиотеки для запросов
+import requests as req
+from urllib.request import urlopen
+from lxml import etree
+
+# веб-страница
+url = 'https://news.mail.ru/economics/44178288/?frommail=1'
+
+headers = {'Content-Type': 'text/html',}
+
+reqs = req.get(url, headers=headers)
+
+html = reqs.text
+
+with open ('req', 'w', encoding='utf-8') as f:
+    f.write(html)
+f.close()
+
+from bs4 import BeautifulSoup
+
+# пример работы с html
+
+soup = BeautifulSoup("<p>Some<b>bad<i>HTML", "lxml")
+
+# печать с абзацами
+
+print(soup.prettify())
+
+soup.find(text="bad")
+
+soup.p
+
+soup = BeautifulSoup("<tag1>Some<tag2/>bad<tag3>XML", "xml")
+
+print(soup.prettify())
+
+for child in soup.recursiveChildGenerator():
+    if child.name:
+        print(child.name)
+
+root = soup.p
+
+# ищем потомков root
+root_childs = [e.name for e in root.children if e.name is not None]
+print(root_childs)
+
+# получаем всех потомков
+root = soup.body
+root_childs = [e.name for e in root.descendants if e.name is not None]
+print(root_childs)
+
+# веб-страница
+url = 'https://news.mail.ru/economics/44178288/?frommail=1'
+
+reqs = req.get(url)
+
+soup = BeautifulSoup(reqs.text, 'lxml')
+print(soup.title)
+print(soup.title.text)
+# print(soup.title.parent)
