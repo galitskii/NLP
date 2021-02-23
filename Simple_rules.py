464433b837b30eaaa0128db8ffe93493a30ecc60
galitskii <73939009+galitskii@users.noreply.github.com>
2021-02-23 11:05:46 +0300

    add 3-4 rules

--- Simple_rules.py
+++ Simple_rules.py
@@ -13,9 +13,53 @@ cur = myconnection.cursor()
 
 def check(str):
     lst = str.split() # разбиваем предложение на слова
-    
+    # проверяем на наличие ошибок в 4 слова
+    if len(lst) > 3:
+        for i in range(0, len(lst) - 3):
+            cur.execute("select pos, singular, cow from words where word = '%s'" % lst[i])
+            res1 = cur.fetchall()
+            res1 = list(set(res1))
+            cur.execute("select pos, singular, cow from words where word = '%s'" % lst[i + 1])
+            res2 = cur.fetchall()
+            res2 = list(set(res2))
+            cur.execute("select pos, singular, cow from words where word = '%s'" % lst[i + 2])
+            res3 = cur.fetchall()
+            res3 = list(set(res3))
+            cur.execute("select pos, singular, cow from words where word = '%s'" % lst[i + 3])
+            res4 = cur.fetchall()
+            res4 = list(set(res4))
+            if len(res1) > 0:
+                for i1 in res1:
+                    for i2 in res2:
+                        for i3 in res3:
+                            for i4 in res4:
+                                cur.execute("select ans, comm, ex from add4_r where prt_1 = '{}' and sing_1 = '{}' and cow_1 = '{}' and prt_2 = '{}' and sing_2 = '{}' and cow_2 = '{}' and prt_3 = '{}' and sing_3 = '{}' and cow_3 = '{}' and prt_4 = '{}' and sing_4 = '{}' and cow_4 = '{}'".format(i1[0], i1[1], i1[2], i2[0], i2[1], i2[2],i3[0], i3[1], i3[2],i4[0], i4[1], i4[2]))
+                                res = cur.fetchall()
+                                res = list(set(res))
+                                if len(res) > 0:
+                                    return ['N']
+    # проверяем на наличие ошибок в 3 слова
+    if len(lst) > 2:
+        for i in range(0, len(lst) - 2):
+            cur.execute("select pos, singular, cow from words where word = '%s'" % lst[i])
+            res1 = cur.fetchall()
+            res1 = list(set(res1))
+            cur.execute("select pos, singular, cow from words where word = '%s'" % lst[i + 1])
+            res2 = cur.fetchall()
+            res2 = list(set(res2))
+            cur.execute("select pos, singular, cow from words where word = '%s'" % lst[i + 2])
+            res3 = cur.fetchall()
+            res3 = list(set(res3))
+            if len(res1) > 0:
+                for i1 in res1:
+                    for i2 in res2:
+                        for i3 in res3:
+                            cur.execute("select ans, comm, ex from add3_r where prt_1 = '{}' and sing_1 = '{}' and cow_1 = '{}' and prt_2 = '{}' and sing_2 = '{}' and cow_2 = '{}' and prt_3 = '{}' and sing_3 = '{}' and cow_3 = '{}'".format(i1[0], i1[1], i1[2], i2[0], i2[1], i2[2],i3[0], i3[1], i3[2]))
+                            res = cur.fetchall()
+                            res = list(set(res))
+                            if len(res) > 0:
+                                return ['N']
     ans = [] # здесь будут варианты ответа
-    
     for q in range(len(lst) - 1):
         ans1 = [] 
         cur.execute("select pos, singular, cow from words where word = '%s'" % lst[q])
@@ -28,14 +72,13 @@ def check(str):
     
         for i in res_l:
             for j in res_r:
-            #print("select ans from simple_rules where prt_l = '{}' and prt_r = '{}' and sing_l = '{}' and sing_r = '{}'".format(i[0], j[0], i[1], j[1]))
-                cur.execute("select ans from simple_rules where prt_l = '{}' and prt_r = '{}' and sing_l = '{}' and sing_r = '{}' and cow_l = '{}' and cow_r = '{}'".format(i[0], j[0], i[1], j[1], i[2], j[2]))
+                cur.execute("select ans, comm, ex, r_id from simple_rules where prt_l = '{}' and prt_r = '{}' and sing_l = '{}' and sing_r = '{}' and cow_l = '{}' and cow_r = '{}'".format(i[0], j[0], i[1], j[1], i[2], j[2]))
                 res = cur.fetchall()
                 res = list(set(res))
 
                 if len(res) > 0:
                     for k in res:
-                        ans1.append(k[0])                        
+                        ans1.append(k[0]) 
         if 'Y' in ans1:
             ans.append('Y')
         elif 'N' in ans1:
