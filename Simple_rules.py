211d90a50f5873923fbea082e44c66a2c56c8b22
galitskii <73939009+galitskii@users.noreply.github.com>
2021-03-02 11:06:31 +0300

    dot

--- Simple_rules.py
+++ Simple_rules.py
@@ -1,19 +1,42 @@
 """
-здесь реализована функция check, которая на вход принимает строку str, 
-состоящую из слов, являющихся местоимением, существительным или глаголом (без знаков пунктуации),
+здесь реализована функция check, которая на вход принимает строку str,
+состоящую из предложений, которые, в свою очередь, являющихся местоимением, существительным или глаголом (разделённые точкой),
 и проверяет, согласовано ли число в этой строке. 
+
+функция str_p отделяет от слов запятые
 
 После этого функция res обрабатывает данные, полученные check, 
 и принимает решение
 """
-
 import cx_Oracle
+
 myconnection = cx_Oracle.connect('lexis/lexis@93.175.29.93/mipt')
+
 cur = myconnection.cursor()
+
+def str_p(l):
+    l1 = []
+    for i in l:
+        if i.find(',') != -1:
+            l1.append(i[0:-1])
+            l1.append(',')
+        else:
+            l1.append(i)
+    return l1
 
 def check(str):
     lst = str.split() # разбиваем предложение на слова
-    # проверяем на наличие ошибок в 4 слова
+    lst = str_p(lst) # отделяем запятые
+    result = [] # здесь будут варианты ответа
+    #prep = []
+    #pand = []
+    #k = 0
+    #for i in lst:
+    #    if i == ',':
+    #        prep.append(k)
+    #    elif i == 'и':
+    #        pand.append(k)
+    #    k += 1
     if len(lst) > 3:
         for i in range(0, len(lst) - 3):
             cur.execute("select pos, singular, cow from words where word = '%s'" % lst[i])
@@ -33,12 +56,16 @@ def check(str):
                     for i2 in res2:
                         for i3 in res3:
                             for i4 in res4:
-                                cur.execute("select ans, comm, ex from add4_r where prt_1 = '{}' and sing_1 = '{}' and cow_1 = '{}' and prt_2 = '{}' and sing_2 = '{}' and cow_2 = '{}' and prt_3 = '{}' and sing_3 = '{}' and cow_3 = '{}' and prt_4 = '{}' and sing_4 = '{}' and cow_4 = '{}'".format(i1[0], i1[1], i1[2], i2[0], i2[1], i2[2],i3[0], i3[1], i3[2],i4[0], i4[1], i4[2]))
+                                cur.execute("select ans from add4_r where prt_1 = '{}' and sing_1 = '{}' and cow_1 = '{}' and prt_2 = '{}' and sing_2 = '{}' and cow_2 = '{}' and prt_3 = '{}' and sing_3 = '{}' and cow_3 = '{}' and prt_4 = '{}' and sing_4 = '{}' and cow_4 = '{}'".format(i1[0], i1[1], i1[2], i2[0], i2[1], i2[2],i3[0], i3[1], i3[2],i4[0], i4[1], i4[2]))
                                 res = cur.fetchall()
                                 res = list(set(res))
                                 if len(res) > 0:
-                                    return ['N']
-    # проверяем на наличие ошибок в 3 слова
+                                    for r in res:
+                                        result.append(r[0])
+            result = list(set(result))
+            if len(result) > 0:
+                if 'N' in result:
+                    return result
     if len(lst) > 2:
         for i in range(0, len(lst) - 2):
             cur.execute("select pos, singular, cow from words where word = '%s'" % lst[i])
@@ -54,12 +81,16 @@ def check(str):
                 for i1 in res1:
                     for i2 in res2:
                         for i3 in res3:
-                            cur.execute("select ans, comm, ex from add3_r where prt_1 = '{}' and sing_1 = '{}' and cow_1 = '{}' and prt_2 = '{}' and sing_2 = '{}' and cow_2 = '{}' and prt_3 = '{}' and sing_3 = '{}' and cow_3 = '{}'".format(i1[0], i1[1], i1[2], i2[0], i2[1], i2[2],i3[0], i3[1], i3[2]))
+                            cur.execute("select ans from add3_r where prt_1 = '{}' and sing_1 = '{}' and cow_1 = '{}' and prt_2 = '{}' and sing_2 = '{}' and cow_2 = '{}' and prt_3 = '{}' and sing_3 = '{}' and cow_3 = '{}'".format(i1[0], i1[1], i1[2], i2[0], i2[1], i2[2],i3[0], i3[1], i3[2]))
                             res = cur.fetchall()
                             res = list(set(res))
                             if len(res) > 0:
-                                return ['N']
-    ans = [] # здесь будут варианты ответа
+                                for r in res:
+                                    result.append(r)
+            result = list(set(result))
+            if len(result) > 0:
+                if 'N' in result:
+                    return result
     for q in range(len(lst) - 1):
         ans1 = [] 
         cur.execute("select pos, singular, cow from words where word = '%s'" % lst[q])
@@ -80,15 +111,15 @@ def check(str):
                     for k in res:
                         ans1.append(k[0]) 
         if 'Y' in ans1:
-            ans.append('Y')
+            result.append('Y')
         elif 'N' in ans1:
-            ans.append('N')
+            result.append('N')
         else:
-            ans.append('E')
-    print("Результат прохода: ", ans)
-    return set(ans)
+            result.append('E')
+    print("Результат прохода: ", result)
+    return set(result)
 
-def res(str):
+def spl(str):
     l = check(str)
     if 'E' in l:
         print("Есть незнакакомые сочетания")
@@ -96,6 +127,14 @@ def res(str):
         print("Ошибка в согласовании единственного и множественного числа")
     else:
         print("Ошибок в согласовании единственного и множественного числа нет")
+
+def res(str):
+    l = str.split(".")
+    for i in l:
+        if i.split() != []:
+            spl(i)
+
+
 
 print("Введите предложение из слов, являющихся местоимением, существительным или глаголом без использования знаков пунктуации:")
 str1 = input()
